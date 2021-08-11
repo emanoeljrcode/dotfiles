5c62b7bd2172d53b093328c46a19e13f90f2a885
Emanoel <emchavesjr@gmail.com>
2021-08-11 15:26:02 -0300

    Add plugged and solving some bugs in install script

--- install.sh
+++ install.sh
@@ -2,16 +2,29 @@
 
 if [ ! -d "$HOME/.ejdots" ]; then
 	echo "Installing EJDots for the first time..." 
-	git clone https://github.com/emanoeljrcode/dotfiles.git "$HOME/.ejdots"
-	if [ ! -d "$HOME/.zshrc" ]; then
-		mv $HOME/.zshrc $HOME/.zshrc.backup
-		ln -s $HOME/.ejdots/.zshrc $HOME
-	fi
 
-	if [ ! -d "$HOME/.tmux.conf" ]; then
-		mv $HOME/.tmux.conf $HOME/.tmux.conf.backup
-		ln -s $HOME/.ejdots/.tmux.conf $HOME
+	git clone https://github.com/emanoeljrcode/dotfiles.git "$HOME/.ejdots"
+
+	if [ -f "$HOME/.zshrc" ]; then
+		mv $HOME/.zshrc $HOME/.zshrc.backup
 	fi
+	ln -s $HOME/.ejdots/.zshrc $HOME
+
+	if [ -f "$HOME/.vimrc" ]; then
+		mv $HOME/.vimrc $HOME/.vimrc.backup
+	fi
+	ln -s $HOME/.ejdots/.vimrc $HOME
+
+	if [ -d "$HOME/.vim" ]; then
+		mv $HOME/.vim $HOME/.vim.backup
+	fi
+	ln -s $HOME/.ejdots/.vim $HOME
+
+	if [ -f "$HOME/.tmux.conf" ]; then
+		mv $HOME/.tmux.conf $HOME/.tmux.conf.backup
+	fi
+	ln -s $HOME/.ejdots/.tmux.conf $HOME
+
 	echo "EJDots has been successfully installed!"
 else
 	echo "EJDots is already installed."
--- uninstall.sh
+++ uninstall.sh
@@ -1,14 +1,25 @@
 #!/bin/sh
 
-if [ ! -d "$HOME/.zshrc.backup" ]; then
+if [ -f "$HOME/.zshrc.backup" ]; then
 	rm $HOME/.zshrc
 	mv $HOME/.zshrc.backup $HOME/.zshrc
 fi
 
-if [ ! -d "$HOME/.tmux.conf.backup" ]; then
+if [ -f "$HOME/.vimrc.backup" ]; then
+	rm $HOME/.vimrc
+	mv $HOME/.vimrc.backup $HOME/.vimrc
+fi
+
+if [ -d "$HOME/.vim.backup" ]; then
+	rm $HOME/.vim
+	mv $HOME/.vim.backup $HOME/.vim
+fi
+
+if [ -f "$HOME/.tmux.conf.backup" ]; then
 	rm $HOME/.tmux.conf
 	mv $HOME/.tmux.conf.backup $HOME/.tmux.conf
 fi
 
-rm $HOME/.ejdots -rf
+rm $HOME/.ejdots -rf && cd $HOME
 
+echo "EJDots has been successfully uninstalled"
