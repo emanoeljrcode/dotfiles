c6e96742b6b4ca165793d1d67596579c6ce65af3
Emanoel <emchavesjr@gmail.com>
2021-08-10 23:17:07 -0300

    Solving a bug in install.sh script

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-#if [ ! -d "$HOME/.ejdots" ]; then
-#	echo "Installing EJDots for the first time" 
+if [ ! -d "$HOME/.ejdots" ]; then
+	echo "Installing EJDots for the first time..." 
+	git clone https://github.com/emanoeljrcode/dotfiles.git "$HOME/.ejdots"
 	if [ ! -d "$HOME/.zshrc" ]; then
 		mv $HOME/.zshrc $HOME/.zshrc.backup
 		ln -s $HOME/.ejdots/.zshrc $HOME
@@ -11,6 +12,7 @@
 		mv $HOME/.tmux.conf $HOME/.tmux.conf.backup
 		ln -s $HOME/.ejdots/.tmux.conf $HOME
 	fi
-#else
-#	echo "EJDots is already installed"
-#fi
+	echo "EJDots has been successfully installed!"
+else
+	echo "EJDots is already installed."
+fi
